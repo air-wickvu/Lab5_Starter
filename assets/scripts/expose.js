7f56c82a9741c0c4a69ec367f84a0784bd8b4c54
Eric Vu <56463764+air-wickvu@users.noreply.github.com>
2023-05-04 16:47:17 -0700

    udpate

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,68 +2,75 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
-const hornSelect = document.getElementById('horn-select');
-const volumeSlider = document.getElementById('volume-slider');
-const audio = document.querySelector('audio');
-const playButton = document.querySelector('button');
-const hornImage = document.querySelector('img');
-
 function init() {
-  hornSelect.addEventListener('change', updateHorn);
-  volumeSlider.addEventListener('input', updateVolume);
-  playButton.addEventListener('click', playSound);
-}
-
-function updateHorn() {
-  const horn = hornSelect.value;
-  const hornData = getHornData(horn);
-  hornImage.src = hornData.image;
-  audio.src = hornData.sound;
-}
-
-function getHornData(horn) {
-  const horns = {
-    airHorn: {
-      image: 'assets/images/air-horn.svg',
-      sound: 'assets/audio/air-horn.mp3'
-    },
-    carHorn: {
-      image: 'assets/images/car-horn.svg',
-      sound: 'assets/audio/car-horn.mp3'
-    },
-    partyHorn: {
-      image: 'assets/images/party-horn.svg',
-      sound: 'assets/audio/party-horn.mp3'
+  // TODO: Set up event listeners and fetch elements
+  const hornSelect = document.getElementById("horn-select");
+  const volumeSlider = document.getElementById("volume");
+  const playButton = document.querySelector("button");
+  const hornImage = document.querySelector("img");
+  const audioElement = document.querySelector("audio");
+  const volumeIcons = document.querySelectorAll("div > img");
+  
+  // Load audio files
+  const airHornSound = new Audio("assets/audio/air-horn.mp3");
+  const carHornSound = new Audio("assets/audio/car-horn.mp3");
+  const partyHornSound = new Audio("assets/audio/party-horn.mp3");
+  
+  // Set volume icons
+  const volumeIconArray = Array.from(volumeIcons);
+  const volume0Icon = volumeIconArray[0];
+  const volume1Icon = volumeIconArray[1];
+  const volume2Icon = volumeIconArray[2];
+  
+  // Set horn image and audio based on selection
+  hornSelect.addEventListener("change", () => {
+    const selectedHorn = hornSelect.value;
+    switch (selectedHorn) {
+      case "air-horn":
+        hornImage.src = "assets/images/air-horn.svg";
+        audioElement.src = airHornSound.src;
+        break;
+      case "car-horn":
+        hornImage.src = "assets/images/car-horn.svg";
+        audioElement.src = carHornSound.src;
+        break;
+      case "party-horn":
+        hornImage.src = "assets/images/party-horn.svg";
+        audioElement.src = partyHornSound.src;
+        break;
+      default:
+        break;
     }
-  };
-  return horns[horn];
-}
-
-function updateVolume() {
-  const volume = volumeSlider.value;
-  const volumeIcon = document.getElementById('volume-image');
-  if (volume == 0) {
-    volumeIcon.src = 'assets/icons/volume-level-0.svg';
-    playButton.disabled = true;
-  } else {
-    playButton.disabled = false;
-    if (volume < 33) {
-      volumeIcon.src = 'assets/icons/volume-level-1.svg';
-    } else if (volume < 67) {
-      volumeIcon.src = 'assets/icons/volume-level-2.svg';
+  });
+  
+  volumeSlider.addEventListener('input', () => {
+    // Set the volume icon based on the volume level
+    const volumeLevel = volumeSlider.value;
+    if (volumeLevel == 0) {
+      volumeIcon.src = './assets/icons/volume-level-0.svg';
+      honkButton.disabled = true; // disable the honk button if volume is muted
+    } else if (volumeLevel < 33) {
+      volumeIcon.src = './assets/icons/volume-level-1.svg';
+      honkButton.disabled = false;
+    } else if (volumeLevel < 67) {
+      volumeIcon.src = './assets/icons/volume-level-2.svg';
+      honkButton.disabled = false;
     } else {
-      volumeIcon.src = 'assets/icons/volume-level-3.svg';
+      volumeIcon.src = './assets/icons/volume-level-3.svg';
+      honkButton.disabled = false;
     }
-  }
-  audio.volume = volume / 100;
-}
-
-function playSound() {
-  const horn = hornSelect.value;
-  const hornData = getHornData(horn);
-  audio.src = hornData.sound;
-  audio.play();
-  if (horn == 'partyHorn') {
-    confetti.start();
-  }
+      // Set the volume of the audio element
+      audio.volume = volumeLevel / 100;
+    });
+  
+  // Play audio and display confetti if Party Horn is selected
+  playButton.addEventListener("click", () => {
+    audioElement.play();
+    if (hornSelect.value === "party-horn") {
+      confetti.start();
+      setTimeout(() => {
+        confetti.stop();
+      }, 3000);
+    }
+  });
 }
